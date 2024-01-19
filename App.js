209c8c8873f6a67e6c4814c6712a1c1ed53fe5a5
radeesh88 <143119605+radeesh88@users.noreply.github.com>
2024-01-19 09:47:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreatePost from './components/Createpost';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <CreatePost />
+    </div>
+  );
+}
+
+export default App;
